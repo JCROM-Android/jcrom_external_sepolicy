9b9b4a457ab43bfa51e14910371e8b10ff9c44c9
Alice Chu <alice.chu@sta.samsung.com>
2012-10-29 17:35:14 -0700

    Moved Android policy tools to tools directory
        - Rename check_seapp to tools directory
        - Move checkfc.c to tools directory
        - Modify check_seapp.c: change 0 to NULL Byte
    
    Change-Id: Iccb05aa3700d75facf18380e84b09fbaea52e6e7

---
+++ tools/Android.mk
@@ -0,0 +1,28 @@
+##
+# checkseapp and checkfc
+#
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := checkseapp
+LOCAL_MODULE_TAGS := optional
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../libsepol/include/ 
+LOCAL_CFLAGS := -DLINK_SEPOL_STATIC 
+LOCAL_SRC_FILES := tools/check_seapp.c
+LOCAL_STATIC_LIBRARIES := libsepol
+LOCAL_MODULE_CLASS := EXECUTABLES
+
+include $(BUILD_HOST_EXECUTABLE)
+
+###################################
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := checkfc
+LOCAL_MODULE_TAGS := optional
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../libsepol/include \
+                    $(LOCAL_PATH)/../libselinux/include
+LOCAL_SRC_FILES := tools/checkfc.c
+LOCAL_STATIC_LIBRARIES := libsepol libselinux
+LOCAL_MODULE_CLASS := EXECUTABLES
+
+include $(BUILD_HOST_EXECUTABLE)
