d870b6ed50bd777319fd55c8a0a2a9181feb50f7
Alice Chu <alice.chu@sta.samsung.com>
2012-10-31 13:14:32 -0700

    Moved Android policy tools to tools directory
    Change-Id: I3bd83fb5a71845975bf1a1e543f23f1a4cd9e3e2

---
+++ tools/Android.mk
@@ -0,0 +1,22 @@
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := checkseapp
+LOCAL_MODULE_TAGS := optional
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../libsepol/include/
+LOCAL_CFLAGS := -DLINK_SEPOL_STATIC
+LOCAL_SRC_FILES := tools/check_seapp.c
+LOCAL_STATIC_LIBRARIES := libsepol
+
+include $(BUILD_HOST_EXECUTABLE)
+
+###################################
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := checkfc
+LOCAL_MODULE_TAGS := optional
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../libsepol/include \
+                    $(LOCAL_PATH)/../libselinux/include
+LOCAL_SRC_FILES := tools/checkfc.c
+LOCAL_STATIC_LIBRARIES := libsepol libselinux
+
+include $(BUILD_HOST_EXECUTABLE)
